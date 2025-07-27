54489c5670ce21589fb6749f7a184f049c0c78b5
ThalisVN <Thalisvn2003@gmail.com>
2025-07-27 13:16:40 -0300

    update 0.7

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
 function toggleMenu() {
-    document.getElementById("icon").classList.toggle("open");
+  const icon = document.getElementById("icon");
+  const menu = document.getElementById("menu");
+
+  icon.classList.toggle("open");
+
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    setTimeout(() => {
+      menu.classList.add("opacity-100", "translate-y-0");
+    }, 10);
+  } else {
+    menu.classList.remove("opacity-100", "translate-y-0");
+    setTimeout(() => {
+      menu.classList.add("hidden");
+    }, 300);
+  }
 }
